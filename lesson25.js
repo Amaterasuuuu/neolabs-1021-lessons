5e82b72bd876cb42740abc7555e4649df2183d94
amaterasuuuu <foreverwakandaa@gmail.com>
2022-01-11 19:32:28 +0600

    deleted unusable code

--- lesson25.js
+++ lesson25.js
@@ -1,14 +1,9 @@
-function sum(...items) { // 2.0.1  
+function sum(...items) {
   return items.length > 0
     ? items.reduce((a, b) => a + b)
     : 0
 }
-// я поменял работу 
+
 console.log(sum(2, 3))
 console.log(sum(2, 3, 4, 5, 6, 7))
 console.log(sum())
-
-
-// git remote add origin git@github.com:neolabsjs/js-course-1021.git
-// git remote set-url origin https://github.com/neolabsjs/js-course-1021.git
-
